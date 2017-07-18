f3469f4273f8668b2617748acfcce0ecdbd988e1
frankli1009 <frankli1009@gmail.com>
2017-07-18 16:31:56 +0100

    Add files via upload

---
+++ FrankLi/js/myScript.js
@@ -0,0 +1,11 @@
+/* =====================================================
+      MOBILE DEVICE MENU SCRIPT
+===================================================== */
+
+$("span.navBtn").click(function(){
+    //$("ul.menu-items").slideToggle();
+    $("#mainMenu").slideToggle();
+});
+
+
+
